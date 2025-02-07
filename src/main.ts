0ca2ad93faaf16c8719199a25bd0ef4bc3b5952e
Lukas Stracke <lukas.stracke@sentry.io>
2025-02-07 14:13:43 +0100

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import "./style.css";
+
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: "https://33f3f99d7064495b95ccacfb9225bbbf@o447951.ingest.us.sentry.io/4504689757257728",
+  tracesSampleRate: 1.0,
+  integrations: [Sentry.browserTracingIntegration()],
+  beforeSend: (event) => {
+    Sentry.startSpan(
+      {
+        name: "error-span",
+        attributes: {
+          "exit.reason": "your-reason-here",
+          // if you want, you can also add the trace_id to the error
+          "error.trace_id": event.contexts?.trace?.trace_id,
+        },
+      },
+      () => {}
+    );
+    return event;
+  },
+});
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <button id="btn">Throw Error</button>
+`;
+
+const errorBtn = document.querySelector<HTMLButtonElement>("#btn")!;
+errorBtn.addEventListener("click", () => {
+  throw new Error("Test Error");
+});
